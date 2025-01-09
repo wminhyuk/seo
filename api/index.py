d03207e894142eb7c6f175d0abd2e1a9ab741bf6
wminhyuk <seoseo1998@gmail.com>
2025-01-09 15:10:43 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,28 +2,36 @@ from fastapi import FastAPI
 from datetime import datetime, date
 from typing import Dict
 import random
-
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
-
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
-
 @app.get("/api/py/ageCalculator/{birthday}")
 def age_calculator(birthday: str) -> Dict[str, str]:
     """
     생년월일을 입력받아 만나이를 계산하는 API
-
     :param birthday: 생년월일 (형식: YYYY-MM-DD)
     :return: 생년월일 및 만나이를 포함한 JSON 응답
     """
-    random_age = random.randint(0, 100)
     today = date.today()
-    
+    birth_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+    age = today.year - birth_date.year
+    # 생일 반영 코드
+    if (today.month, today.day) < (birth_month, birth_day):
+        age -= 1
+
     return {
             "birthday": birthday,
-            "age": str(26),
+<<<<<<< HEAD
+            "age": str(age),
+=======
+<<<<<<< HEAD
+            "age": str(age),
+=======
+            "age": str(age),
+>>>>>>> d8c78a38faf01a9d7e11222d52680b8d9759baa2
+>>>>>>> 138c3576efeef0115d4234acfc49ec48a02814e4
             "basedate": str(today),
             "message": "Age calculated successfully!"
             }
