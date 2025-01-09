05da859f7b3ab2438e83bd4ede29f6fadc49e600
wminhyuk <seoseo1998@gmail.com>
2025-01-09 16:49:57 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from datetime import datetime, date
 from typing import Dict
+import random
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
@@ -10,7 +11,6 @@ def hello_fast_api():
     return {"message": "Hello from FastAPI"}
 
 @app.get("/api/py/ageCalculator/{birthday}")
-
 def age_calculator(birthday: str) -> Dict[str, str]:
     """
     생년월일을 입력받아 만나이를 계산하는 API
@@ -20,11 +20,12 @@ def age_calculator(birthday: str) -> Dict[str, str]:
     """
     today = date.today()
     birth_date = datetime.strptime(birthday, "%Y-%m-%d").date()
-    age  = today.year - birth_date.year
-    
-    # FiX 생일 지난 여부 확인
-    if (birth_date.month > today.month) or (birth_date.month == today.month and birth_date.day > today.day):
+    age = today.year - birth_date.year
+
+    # 생일 반영 코드
+    if (today.month, today.day) < (birth_month, birth_day):
         age -= 1
+
 
     return {
             "birthday": birthday,
